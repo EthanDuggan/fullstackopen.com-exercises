a1b101c2e3fa98ab3ba0cebc5317f067b7306a6a
Ethan Duggan <eduggan1997@gmail.com>
2023-03-04 15:27:38 -0500

    completed exercise 1.4

--- part1/courseinfo/my-app/src/App.js
+++ part1/courseinfo/my-app/src/App.js
@@ -27,37 +27,33 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <p>
-      Number of exercises {props.exercises}
+      Number of exercises {props.parts.reduce((exercises, part) => exercises + part.exercises, 0)}
     </p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
   const parts = [
-    part1,
-    part2,
-    part3,
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    },
   ]
 
   return (
     <div>
       <Header course={course} />
       <Content parts={parts} />
-      <Total exercises={parts.reduce((exercises, part) => exercises + part.exercises, 0)} />
+      <Total parts={parts} />
     </div>
   )
 }
