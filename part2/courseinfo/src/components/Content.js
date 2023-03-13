ca8779d77edda34fa9184697b94ec1915ede9b1a
Ethan Duggan <eduggan1997@gmail.com>
2023-03-13 13:47:46 -0400

    completed exercise 2.4 and changed Content.js to use part.id as the keys for the Part components rather than part.name

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -2,7 +2,7 @@ import Part from './Part'
 
 const Content = ({parts}) => {
     const partComponents = parts.map(part => (
-      <Part key={part.name} name={part.name} exercises={part.exercises} />
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
     ))
   
     return (
