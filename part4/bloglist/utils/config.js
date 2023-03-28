7b2d7c1bde9335e26050c4d8793be48493f2a229
Ethan Duggan <eduggan1997@gmail.com>
2023-03-28 14:44:43 -0400

    completed exercise 4.2

---
+++ part4/bloglist/utils/config.js
@@ -0,0 +1,7 @@
+const PORT = 3003
+const mongoUrl = 'mongodb+srv://eduggan1997:u9iLmVmot2pUDnmY@mycluster.uyxvzww.mongodb.net/bloglist?retryWrites=true&w=majority'
+
+module.exports = {
+	mongoUrl,
+	PORT,
+}
