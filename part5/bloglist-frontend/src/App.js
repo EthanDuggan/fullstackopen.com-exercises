789d32cb2e33a141dc34cada9cd5d3cc98266eed
Ethan Duggan <eduggan1997@gmail.com>
2023-04-06 14:49:16 -0400

    completed exercise 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,22 +1,57 @@
 import { useState, useEffect } from 'react'
+
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+  // DECLARE STATE HOOKS
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
+  // DECLARE EFFECT HOOKS
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
+  // DECLARE EVENT HANDLERS
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username,
+        password
+      })
+
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      console.error('wrong credentials')
+    }
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+
+      <h2>login</h2>
+      
+      {!user && <LoginForm handleLogin={handleLogin} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />}
+
+      {user && <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>}
     </div>
   )
 }
